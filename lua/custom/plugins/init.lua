ae2132552743be46bb07f2fef312f9d306e2547c
souheil-moussa <souheil.imoussa@gmail.com>
2024-11-30 07:26:07 +0200

    added nvim tree and web icons

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,6 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-	"scottmckendry/cyberdream.nvim",
-	lazy = false,
-	priority = 1000,
+
+	'nvim-tree/nvim-web-devicons',
+
 }
+
